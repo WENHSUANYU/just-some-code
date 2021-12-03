3ab56cfa7ceb0e71d50cdaa570f436cee41b69e5
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2021-12-03 17:25:24 +0800

    Update a FibonacciNumberRecursiveVersion.cpp

--- DP/FibonacciNumberRecursiveVersion.cpp
+++ DP/FibonacciNumberRecursiveVersion.cpp
@@ -1,11 +1,24 @@
 #include<iostream>
+#include<array>
 using namespace std;
+// n <= 45
+
+int fib(int n, array<int, 50> &F) {
+    if (n <= 1)
+        return F[n] = 1;
+    if (F[n] != -1)
+        return F[n];
+    return F[n] = fib(n - 1, F) + fib(n - 2, F);
+}
+
 
 int main(void)
 {
-
-
-
+    int n;
+    cin >> n;
+    array<int, 50>F;
+    F.fill(-1);
+    cout << fib(n, F) << endl;
 
     return 0;
 }
